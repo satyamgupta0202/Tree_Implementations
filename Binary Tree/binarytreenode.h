58ecf4360642abcda24c2d385ec66c9156b5afb3
satyam <2019335@iiitdmj.ac.in.com>
2021-03-02 11:24:35 +0530

    binary tree node

---
+++ Binary Tree/binarytreenode.h
@@ -0,0 +1,20 @@
+using namespace std;
+
+template <typename t>
+
+class binarytreenode {
+    public:
+    t data;
+    binarytreenode* left;
+    binarytreenode* right;
+
+    binarytreenode(t data){
+        this->data=data;
+        left=null;
+        right=null;
+    }
+    ~binarytreenode(){      //destructor
+        delete left;
+        delete right;
+    }
+}
