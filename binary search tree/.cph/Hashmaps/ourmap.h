080b48efd5f16416366d746463e4d52754a37355
satyam <2019335@iiitdmj.ac.in.com>
2021-03-05 12:33:59 +0530

    Insertion_hashmap

--- binary search tree/.cph/Hashmaps/ourmap.h
+++ binary search tree/.cph/Hashmaps/ourmap.h
@@ -44,8 +44,64 @@ class ourmap{
         delete []buckets;
     }
 
+ ///////////////////functions/////////////////////
+
+ //1//
+
     int size(){
         return count;
     }
+
+//2//
+
+    v getValue(string key){
+
+    }
+
+private:
+    int getbucketIndex(string key){
+        int hashcode=0;
+        int currentcoff=1;
+
+        for(int i=k.size()-1;i>=0;i--){
+            hashcode+=key[i]*currentcoff;
+            hashcode = hashcode%numbuckets;
+            currentcoff*=37;
+            currentcoff = currentcoff%numbuckets;
+        }
+
+
+
+        return hashcode%numbuckets;
+    }
+//3//
+    public:
+    void insert(string key , v value){
+        int bucketIndex = getbucketIndex(string key);
+        mapnode<v>*head = bucket[bucketIndex];
+        while(head!=NULL){
+            if(head->key == key){
+            head->value = value;
+            return ;
+            }
+            head = head->next;
+        }
+        head = buckets[bucketIndex];
+        mapnode<v>* node = new mapnode(key,value);
+        node->next = head;
+        bucket[bucketIndex]=node;
+        count++;
+    }
+
+//4//
+
+
+    v remove(string key){
+
+    }
+
+       
+
+
 
 };
